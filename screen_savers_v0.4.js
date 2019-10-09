9a5bf2f0a6da427948fb5e8758ed8345dc715719
ZenBoba <54600194+ZenBoba@users.noreply.github.com>
2019-10-09 11:49:33 -0600

    screen_savers_v0.4.js
    updated the code for SS4

--- screen_savers_v0.4.js
+++ screen_savers_v0.4.js
@@ -1,26 +1,46 @@
-let isSleeping = false
-input.onButtonPressed(Button.A, function () {
-    if (input.buttonIsPressed(Button.A)) {
-        isSleeping = !(isSleeping)
-    }
-    while (true) {
-        if (input.isGesture(Gesture.TiltLeft)) {
-            basic.clearScreen()
-            Screen_saver1()
-        } else if (input.isGesture(Gesture.Shake)) {
-            basic.clearScreen()
-            Screen_saver2()
-        } else if (input.isGesture(Gesture.TiltRight)) {
-            basic.clearScreen()
-            Screen_saver3()
-        } else if (input.isGesture(Gesture.LogoDown)) {
-            basic.clearScreen()
-            Screen_saver4()
-        } else {
-            basic.showNumber(2)
-        }
-    }
+input.onButtonPressed(Button.B, function () {
+    isSleeping = false
+    led.stopAnimation()
+    basic.clearScreen()
+    basic.showString("WAKEY WAKEY")
 })
+input.onButtonPressed(Button.A, function () {
+    isSleeping = true
+    led.stopAnimation()
+    basic.clearScreen()
+    basic.showString("SLEEPY TIME")
+})
+function Screen_saver4() {
+    for (let i = 0; i < 1; i++) {
+        led.plot(0, 2)
+        basic.pause(200)
+        led.plot(1, 1)
+        led.unplot(0, 2)
+        basic.pause(200)
+        led.plot(2, 2)
+        led.unplot(1, 1)
+        basic.pause(200)
+        led.plot(3, 3)
+        led.unplot(2, 2)
+        basic.pause(200)
+        led.plot(4, 2)
+        led.unplot(3, 3)
+        basic.pause(200)
+        led.plot(3, 1)
+        led.unplot(4, 2)
+        basic.pause(200)
+        led.plot(2, 2)
+        led.unplot(3, 1)
+        basic.pause(200)
+        led.plot(1, 3)
+        led.unplot(2, 2)
+        basic.pause(200)
+        led.plot(0, 2)
+        led.unplot(1, 3)
+        basic.pause(50)
+        led.unplot(0, 2)
+    }
+}
 function Screen_saver1() {
     led.plot(2, 0)
     basic.pause(200)
@@ -64,7 +84,7 @@ function Screen_saver1() {
     led.fadeIn(500)
 }
 function Screen_saver2() {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 1; i++) {
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -96,7 +116,7 @@ function Screen_saver2() {
     }
 }
 function Screen_saver3() {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 1; i++) {
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -120,35 +140,27 @@ function Screen_saver3() {
             `)
     }
 }
-function Screen_saver4() {
-    for (let i = 0; i < 2; i++) {
-        led.plot(0, 2)
-        basic.pause(200)
-        led.plot(1, 1)
-        led.unplot(0, 2)
-        basic.pause(200)
-        led.plot(2, 2)
-        led.unplot(1, 1)
-        basic.pause(200)
-        led.plot(3, 3)
-        led.unplot(2, 2)
-        basic.pause(200)
-        led.plot(4, 2)
-        led.unplot(3, 3)
-        basic.pause(200)
-        led.plot(3, 1)
-        led.unplot(4, 2)
-        basic.pause(200)
-        led.plot(2, 2)
-        led.unplot(3, 1)
-        basic.pause(200)
-        led.plot(1, 3)
-        led.unplot(2, 2)
-        basic.pause(200)
-        led.plot(0, 2)
-        led.unplot(1, 3)
-        basic.pause(50)
-        led.unplot(0, 2)
+let isSleeping: boolean = false
+basic.forever(function () {
+    while (isSleeping) {
+        if (input.isGesture(Gesture.TiltLeft)) {
+            basic.clearScreen()
+            Screen_saver1()
+        } else if (input.isGesture(Gesture.Shake)) {
+            basic.clearScreen()
+            Screen_saver2()
+        } else if (input.isGesture(Gesture.TiltRight)) {
+            basic.clearScreen()
+            Screen_saver3()
+        } else if (input.isGesture(Gesture.LogoDown)) {
+            basic.clearScreen()
+            Screen_saver4()
+        } else {
+        }
     }
-}
- 
+    while (!(isSleeping)) {
+        basic.clearScreen()
+        basic.showString("IM AWAKE")
+    }
+})
+
