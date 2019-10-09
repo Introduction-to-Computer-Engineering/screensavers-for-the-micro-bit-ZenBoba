b1d4406c0e9cb6d114735c40afa246ad6f4d1331
ZenBoba <54600194+ZenBoba@users.noreply.github.com>
2019-10-09 11:52:15 -0600

    screen_savers_v0.5.js
    updated code to SS5

--- screen_savers_v0.5.js
+++ screen_savers_v0.5.js
@@ -1,128 +1,17 @@
-let isSleeping = false
-input.onButtonPressed(Button.A, function () {
-    if (input.buttonIsPressed(Button.A)) {
-        isSleeping = !(isSleeping)
-    }
-    while (true) {
-        if (input.isGesture(Gesture.TiltLeft)) {
-            basic.clearScreen()
-            Screen_saver1()
-        } else if (input.isGesture(Gesture.Shake)) {
-            basic.clearScreen()
-            Screen_saver2()
-        } else if (input.isGesture(Gesture.TiltRight)) {
-            basic.clearScreen()
-            Screen_saver3()
-        } else if (input.isGesture(Gesture.LogoDown)) {
-            basic.clearScreen()
-            Screen_saver4()
-        } else {
-            basic.clearScreen()
-            Screen_saver5()
-        }
-    }
+input.onButtonPressed(Button.B, function () {
+    isSleeping = false
+    led.stopAnimation()
+    basic.clearScreen()
+    basic.showString("WAKEY WAKEY")
 })
-function Screen_saver1 () {
-    led.plot(2, 0)
-    basic.pause(200)
-    led.plot(2, 1)
-    basic.pause(200)
-    led.plot(2, 2)
-    basic.pause(200)
-    led.plot(2, 3)
-    basic.pause(200)
-    led.plot(2, 4)
-    led.fadeOut(500)
-led.unplot(2, 0)
-    basic.pause(200)
-    led.unplot(2, 1)
-    basic.pause(200)
-    led.unplot(2, 2)
-    basic.pause(200)
-    led.unplot(2, 3)
-    basic.pause(200)
-    led.unplot(2, 4)
-    led.fadeIn(600)
-led.plot(0, 2)
-    basic.pause(200)
-    led.plot(1, 2)
-    basic.pause(200)
-    led.plot(2, 2)
-    basic.pause(200)
-    led.plot(3, 2)
-    basic.pause(200)
-    led.plot(4, 2)
-    led.fadeOut(500)
-led.unplot(0, 2)
-    basic.pause(200)
-    led.unplot(1, 2)
-    basic.pause(200)
-    led.unplot(2, 2)
-    basic.pause(200)
-    led.unplot(3, 2)
-    basic.pause(200)
-    led.unplot(4, 2)
-    led.fadeIn(500)
-}
-function Screen_saver2 () {
-    for (let i = 0; i < 4; i++) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            . . # . .
-            . # . . .
-            # . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            # . . . .
-            . # . . .
-            . . # . .
-            . . . # .
-            . . . . #
-            `)
-    }
-}
-function Screen_saver3 () {
-    for (let i = 0; i < 4; i++) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # . # .
-            . # # # .
-            . . . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # . . . #
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
-    }
-}
-function Screen_saver4 () {
-    for (let i = 0; i < 2; i++) {
+input.onButtonPressed(Button.A, function () {
+    isSleeping = true
+    led.stopAnimation()
+    basic.clearScreen()
+    basic.showString("SLEEPY TIME")
+})
+function Screen_saver4() {
+    for (let i = 0; i < 1; i++) {
         led.plot(0, 2)
         basic.pause(200)
         led.plot(1, 1)
@@ -152,8 +41,8 @@ function Screen_saver4 () {
         led.unplot(0, 2)
     }
 }
-function Screen_saver5 () {
-    for (let i = 0; i < 4; i++) {
+function Screen_saver5() {
+    for (let i = 0; i < 1; i++) {
         basic.pause(300)
         basic.showLeds(`
             # # # # #
@@ -194,3 +83,128 @@ function Screen_saver5 () {
         led.fadeIn()
     }
 }
+function Screen_saver1() {
+    led.plot(2, 0)
+    basic.pause(200)
+    led.plot(2, 1)
+    basic.pause(200)
+    led.plot(2, 2)
+    basic.pause(200)
+    led.plot(2, 3)
+    basic.pause(200)
+    led.plot(2, 4)
+    led.fadeOut(500)
+    led.unplot(2, 0)
+    basic.pause(200)
+    led.unplot(2, 1)
+    basic.pause(200)
+    led.unplot(2, 2)
+    basic.pause(200)
+    led.unplot(2, 3)
+    basic.pause(200)
+    led.unplot(2, 4)
+    led.fadeIn(600)
+    led.plot(0, 2)
+    basic.pause(200)
+    led.plot(1, 2)
+    basic.pause(200)
+    led.plot(2, 2)
+    basic.pause(200)
+    led.plot(3, 2)
+    basic.pause(200)
+    led.plot(4, 2)
+    led.fadeOut(500)
+    led.unplot(0, 2)
+    basic.pause(200)
+    led.unplot(1, 2)
+    basic.pause(200)
+    led.unplot(2, 2)
+    basic.pause(200)
+    led.unplot(3, 2)
+    basic.pause(200)
+    led.unplot(4, 2)
+    led.fadeIn(500)
+}
+function Screen_saver2() {
+    for (let i = 0; i < 1; i++) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . #
+            `)
+    }
+}
+function Screen_saver3() {
+    for (let i = 0; i < 1; i++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+}
+let isSleeping: boolean = false
+basic.forever(function () {
+    while (isSleeping) {
+        if (input.isGesture(Gesture.TiltLeft)) {
+            basic.clearScreen()
+            Screen_saver1()
+        } else if (input.isGesture(Gesture.Shake)) {
+            basic.clearScreen()
+            Screen_saver2()
+        } else if (input.isGesture(Gesture.TiltRight)) {
+            basic.clearScreen()
+            Screen_saver3()
+        } else if (input.isGesture(Gesture.LogoDown)) {
+            basic.clearScreen()
+            Screen_saver4()
+        } else {
+            basic.clearScreen()
+            Screen_saver5()
+        }
+    }
+    while (!(isSleeping)) {
+        basic.clearScreen()
+        basic.showString("IM AWAKE")
+    }
+})
+
